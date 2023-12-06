758d56854a2e0779341181d02885c2d07ca6cc59
marciorps <marciorps@yahoo.com,.br>
2023-12-06 16:17:44 -0300

    ordenar lista simples

---
+++ ordenar_lista_simples.py
@@ -0,0 +1,8 @@
+valores = [31,23,6,36,21,33,37,7,20,23]
+#Ordenador de lista em ordem crescente
+valores.sort()
+#ordenador em ordem decrescente
+# valores.sort(reverse=True)
+# #Inverter uma lista
+# valores.reverse()
+print(valores)
